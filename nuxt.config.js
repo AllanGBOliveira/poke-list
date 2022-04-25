61a8f94e5e8409b0b8db7c22417fbec2ad8236e0
AllanGBOliveira <allebeira@gmail.com>
2022-04-25 17:46:17 -0300

    adicionado gb pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/poke-list/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
