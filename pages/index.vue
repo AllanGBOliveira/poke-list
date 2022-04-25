cf474ec7f1e71770dae99faf1baab0238ae54b10
AllanGBOliveira <allebeira@gmail.com>
2022-04-25 17:48:34 -0300

    alterado index

--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,7 @@
 <script>
 import ListPokemon from '../components/ListPokemon.vue'
 export default {
-    components: { ListPokemon },
+  components: { ListPokemon },
   name: 'IndexPage',
-  
-  async asyncData({ $axios }) {
-  const pokemons = await $axios.$get('http://icanhazip.com')
-  return { pokemons }
-},
-
 }
 </script>
